09707c9c95c30aeebab3a56f95dc779109ddbd30
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-01-16 23:44:52 +0100

    feat: created basic form

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,3 +1,4 @@
+import CreateAnAccount from "@/components/UI/Home/create-account";
 import Image from "next/image";
 
 const Page = () => {
@@ -14,12 +15,16 @@ const Page = () => {
 
         <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-black flex items-center justify-center">
           <div className="text-center">
-            <h1 className="text-6xl font-extrabold">Smartlink</h1>
+            <h1 className="text-6xl font-extrabold">
+              Smart<span className="text-cyan-300">link</span>
+            </h1>
           </div>
         </div>
       </header>
       <main>
-        <section className="h-screen bg-black"></section>
+        <section className="h-screen bg-black">
+          <CreateAnAccount />
+        </section>
       </main>
     </>
   );
---
+++ components/UI/Home/create-account.tsx
@@ -0,0 +1,80 @@
+"use client";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface Inputs {
+  fullName: string;
+  email: string;
+  password: string;
+  // confirmPassword: string;
+  // phoneNumber: string;
+}
+
+const CreateAnAccount = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="container pt-10">
+      <h2 className="text-center font-extrabold text-3xl opacity-80">Create An Account</h2>
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        noValidate
+        className="max-w-4xl mx-auto mt-5 flex items-center justify-center"
+      >
+        <div className="space-y-3 p-2">
+          <div className="space-y-1">
+            <label htmlFor="fullName" className="font-medium">
+              Full Name
+            </label>
+            <input
+              type="text"
+              className="w-full outline-none p-3 rounded-lg bg-transparent border border-zinc-100/10"
+              placeholder="Your Full Name"
+              autoComplete="off"
+              {...register("fullName", { required: true })}
+            />
+          </div>
+          <div className="space-y-1">
+            <label htmlFor="email" className="font-medium">
+              Email
+            </label>
+            <input
+              type="email"
+              className="w-full outline-none p-3 rounded-lg bg-transparent border border-zinc-100/10"
+              placeholder="Your Email"
+              autoComplete="off"
+              {...register("email", { required: true })}
+            />
+          </div>
+          <div className="space-y-1">
+            <label htmlFor="password" className="font-medium">
+              Password
+            </label>
+            <input
+              type="password"
+              className="w-full outline-none p-3 rounded-lg bg-transparent border border-zinc-100/10"
+              placeholder="Password"
+              autoComplete="off"
+              {...register("password", { required: true })}
+            />
+          </div>
+          <input
+            type="submit"
+            value="Create Account"
+            className="w-full p-3 rounded-lg bg-cyan-900 text-white cursor-pointer duration-300 hover:bg-cyan-800"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateAnAccount;
